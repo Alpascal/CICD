8bc6f8d46fc5cdd68f005e5c3b5ff562dda91ca4
phonse006@gmail.com <phonse006@gmail.com>
2021-01-05 22:40:24 +0000

    voici mon premier commit

---
+++ src/main/java/com/example/mon_projet1/MonProjet1Application.java
@@ -0,0 +1,13 @@
+package com.example.mon_projet1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonProjet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonProjet1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mon_projet1/MonProjet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mon_projet1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonProjet1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
